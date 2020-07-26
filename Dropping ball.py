b66819c20401f86a95b2cf5532d45bb1d3a34d80
Dharma Raj Paudel <67821078+dharmapaudel@users.noreply.github.com>
2020-07-26 12:51:16 +0545

    Add files via upload

---
+++ Dropping ball.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Python fundamentals for Computational Physics
+# A ball dropped from a tower
+
+# In[5]:
+
+
+h = float(input("Enter the height of the tower:"))
+t = float(input("Enter the time interval:"))
+a = 1/2
+g = 9.8
+s = a*g*t**2 #Vertical distance  travelled in time t
+
+
+print("The height of the ball is ", h-s,"meters")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
